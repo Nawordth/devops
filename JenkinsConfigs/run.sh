4b3a1377d77b9640ed3f6a495c64aa4f4786313c
Nawordth <53595539+Nawordth@users.noreply.github.com>
2019-08-28 12:05:22 +0530

    Update run.sh

--- JenkinsConfigs/run.sh
+++ JenkinsConfigs/run.sh
@@ -8,8 +8,10 @@ echo -    $personal_access_token
 echo -
 echo --------------------------------------------------------
 grep -qxF 'chmod 777 /var/run/docker.sock' /etc/rc.d/rc.local || echo 'chmod 777 /var/run/docker.sock' >> /etc/rc.d/rc.local
+dockerhost=$(hostname -I | awk '{print $1}')
 
 docker run \
+ --add-host=dockerhost:$dockerhost
  --name jenkins-sandbox \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -p 8080:8080 \
